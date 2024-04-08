1559af7d693015c1580ee959718a9bee2ac6138b
Wadson <pinheirowadson@gmail.com>
2024-04-08 14:16:44 -0300

    feat: ProductService findAllPagead

--- BackEnd/src/main/java/com/wpCorp/dsCommerce/Repository/ProductRepository.java
+++ BackEnd/src/main/java/com/wpCorp/dsCommerce/Repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.wpCorp.dsCommerce.Repository;
 
 import com.wpCorp.dsCommerce.Entity.ProductEntity;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    Page<ProductEntity> findAllPagead();
 }
--- BackEnd/src/main/java/com/wpCorp/dsCommerce/Service/ProductService.java
+++ BackEnd/src/main/java/com/wpCorp/dsCommerce/Service/ProductService.java
@@ -1,7 +1,11 @@
 package com.wpCorp.dsCommerce.Service;
 
+import com.wpCorp.dsCommerce.DTO.ProductDTO;
+import com.wpCorp.dsCommerce.Entity.ProductEntity;
 import com.wpCorp.dsCommerce.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,5 +14,10 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepo;
 
-   // @Transactional(readOnly = true)
+    @Transactional(readOnly = true)
+    protected Page<ProductDTO> findAllPaged(Pageable page) {
+        Page<ProductEntity> rsl = productRepo.findAllPagead();
+        return rsl.map(ProductDTO::new);
+
+    }
 }
